46f4bd279b2a9d793ccaf41785bd6dbb2e826f62
Marlon Gomes Lopes <marlonglopes@gmail.com>
2010-09-22 13:29:24 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Consultoriamedica::Application.config.secret_token = '09ba55096013285abd562683fe4b50f6842b7d95383ea7f1ce7ffb955d9e82575f604ab76df37c7283d83ca4239ea702e7978a170db3e6b761c28f6153137634'
